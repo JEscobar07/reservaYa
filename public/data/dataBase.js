06402347b85d808fd03fc893645a8ec1fae62eaf
JEscobar07 <EscobarjonathanM@outlook.es>
2024-05-16 19:12:29 -0500

    feat:Se crea todo el esquema de carpetas y documentos basicos como html,main,style entre otros.

---
+++ public/data/dataBase.js
@@ -0,0 +1,5 @@
+/*
+Clan: Clan Gates
+Grupo: Charlie
+Fecha: 16/05/2024
+*/
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/*
+Clan: Clan Gates
+Grupo: Charlie
+Fecha: 16/05/2024
+*/
+
+// Import our custom CSS
+import '../scss/styles.scss'
+
